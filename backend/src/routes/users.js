273071130d1808f9f79e64af81ebab764bc71fff
muna.b <mainmouna.sakho@gmail.com>
2021-08-17 00:33:43 +0200

    add server.js + route users

---
+++ backend/src/routes/users.js
@@ -0,0 +1,70 @@
+const argon2 = require('argon2')
+const createError= require('http-errors')
+
+async function routes(fastify, options) {
+    //#region users
+    const optsPost= {
+        schema: {
+            body: {
+                properties: {
+                    username: { type:'string'},
+                    email: { type:'string'},
+                    password: { type:'string'},
+                    dateOfBirth: { type:'string'},
+                    adress: { type:'string'},
+                },
+                required: ['username', 'email', 'password', 'dateOfBirth', 'adress'],
+                additionnalProperties: false
+            }
+        }
+    }
+    fastify.post('/users', optsPost, async (request, reply) => {
+        const {email, password} = request.body
+        const db = fastify.mongo.db
+        const collection = db.collection('users')
+        const userExist = await collection.findOne({email})
+        if (userExist) throw new createError.Conflict('Email already exists')
+        const hashedPassword = await argon2.hash(password)
+        const result = await collection.insertOne({
+            ...request.body,
+            password: hashedPassword,
+            role: 'user'
+        })
+        return{
+            id: result.ops[0]._id
+        }
+    })
+    //#endregion
+
+    //#region login user
+    const optsLogin = {
+        schema: {
+            body: {
+                type: 'object',
+                properties: {
+                    email: { type: 'string'},
+                    password: { type: 'string'},
+                },
+                required: ['email', 'password'],
+                additionnalProperties: false,
+            }
+        }
+    }
+    fastify.post('/login', optsLogin, async (request, reply) => {
+        const { email, password } = request.body
+        const db = fastify.mongo.db
+        const collection = db.collection('users')
+        const user = await collection.findOne({email})
+            if (!user) throw new createError.NotFound('Wrong email and/or password')
+        const match = await argon2.verify(user.password, password)
+            if (!match) throw new createError.NotFound('Wrong email and/or password')
+        const token = await reply.jwtSign({
+            id: user._id,
+            role: user.role,
+            expireIn: "10m"
+        })
+        reply.code(200).send({token})
+    })
+    //#endregion
+}
+module.exports = routes
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,28 @@
+require('dotenv').config()
+
 const fastify = require('fastify')({
     logger: true
   })
   
-  fastify.get('/', async (request, reply) => {
-    return { hello: 'world' }
-  })
+fastify.register(require('fastify-mongodb'), {
+  forceClose: true,
+  url: process.env.MONGODBURL
+})
+
+fastify.register(require('fastify-jwt'), {
+  secret: process.env.SECRET
+})
+
+fastify.register(require('fastify-cors',), {
+  origin: "*"
+})
   
-  const start = async () => {
-    try {
-      await fastify.listen(3000)
-    } catch (err) {
-      fastify.log.error(err)
-      process.exit(1)
-    }
+const start = async () => {
+  try {
+    await fastify.listen(3001)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
   }
-  start()
+}
+start()
