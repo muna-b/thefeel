bf80b391446f18f9ab3d03fac7b7cc19e3d2c36d
muna-b <mainmouna.sakho@gmail.com>
2021-11-28 13:04:43 +0100

    private.js

--- backend/src/routes/private.js
+++ backend/src/routes/private.js
@@ -1,9 +1,15 @@
-async function routes(fastify, options) {
+async function routes(fastify, options, next) {
 	fastify.addHook('onRequest', fastify.authenticate)
-
+	
 	fastify.get('/private', async (request, reply) => {
-		return 'you are authenticated'
+		try {
+			const params = request.params
+			return params
+		} catch (error) {
+			reply.code(500).send(error)
+		}
 	})
+	next()
 }
 
 module.exports = routes
